c16dc2704562efb59dba557b25d5cf09b9b15de0
Sayantani Manna <sayantanimanna139@gmail.com>
2026-05-03 00:01:54 +0530

    Time: 49 ms (25.18%), Space: 54.1 MB (14.19%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let i
+    let filteredArr = []
+    for (i=0;i<arr.length;i++){
+    if (fn(arr[i],i)){
+
+        filteredArr.push(arr[i])
+
+    }
+    
+    } 
+    return filteredArr 
+};
