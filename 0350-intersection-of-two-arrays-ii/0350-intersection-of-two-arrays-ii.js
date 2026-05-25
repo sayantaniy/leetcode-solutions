2dd0b5db4214899a50d25d6413069f6c10a3a2c0
Sayantani Manna <sayantanimanna139@gmail.com>
2026-05-26 00:57:53 +0530

    Time: 4 ms (26.85%), Space: 56 MB (47.89%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -4,17 +4,30 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    let visited = new Array(nums2.length).fill(0)
-    let i, j;
+
+    nums1 = nums1.sort((a,b)=>a-b)
+    nums2 = nums2.sort((a,b)=>a-b)
+    
+    let i =0, j=0
     let ans = []
-    for (i=0;i<nums1.length;i++){
-        for(j=0;j<nums2.length;j++){
-            if((nums1[i]===nums2[j]) && visited[j]===0){
-                ans.push(nums1[i])
-                visited[j]++
-                break
-            }
+
+    while(i<nums1.length && j<nums2.length){
+
+        if(nums1[i]<nums2[j]){
+            i++
         }
+
+        else if(nums1[i]>nums2[j]){
+            j++
+        }
+
+        else if(nums1[i]===nums2[j]){
+            ans.push(nums1[i])
+            i++
+            j++
+        }
+        
     }
+
     return ans
 };
