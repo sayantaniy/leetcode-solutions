5e000ea9083be239b18205cda1e96fcf550eb1ec
Sayantani Manna <sayantanimanna139@gmail.com>
2026-06-04 23:01:29 +0530

    Time: 0 ms (100%), Space: 56.4 MB (69.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,14 +3,21 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let mp = new Map()
-    //O(nlogn)
-        for(let i=0;i<nums.length;i++){
-            mp.set(nums[i], (mp.get(nums[i]) || 0) + 1)
+    //aplying moore's voting algo which has t.c= O(n) and s.c = O(1)
+    let c=0
+    let elem
+    for(let i=0;i<nums.length;i++){
+        if (c===0){
+            c=1
+            elem=nums[i]
         }
-        //O(n) max
-        for (let [key,val] of mp)
-        if (val>nums.length/2){
-            return key
+        else if (nums[i]===elem){
+            c++
         }
+        else
+        c--
+        //this c does not signify anything
+    }
+    return elem
+    
 };
