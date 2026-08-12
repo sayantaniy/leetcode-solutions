01533b5cbd8ad01cb00e933090fbc9b76faf8b8f
Sayantani Manna <sayantanimanna139@gmail.com>
2026-08-12 23:57:39 +0530

    Time: 0 ms (100%), Space: 53.7 MB (53.43%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    s = s.trim()
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === " ") {
+            return s.length - 1 - i
+        }
+
+    }
+    return s.length
+};
