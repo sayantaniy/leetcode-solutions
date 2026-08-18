89ff06cb408b5538f6ba25fe9301f19baa18601a
Sayantani Manna <sayantanimanna139@gmail.com>
2026-08-18 22:59:30 +0530

    Time: 22 ms (26.89%), Space: 60 MB (20.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,12 +3,20 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    if(s.split('').sort().join() === t.split('').sort().join()) {
-    return true
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) return false
+    let map = new Map()
+    for (let char of s) {
+        (map.set(char, (map.get(char) || 0) + 1))
     }
-    else{
-    return false
+    for (let char of t) {
+        if (!map.get(char)) return false
+
+            map.set(char, (map.get(char) - 1))
+
+        if (map.get(char) === 0)
+            map.delete(char)
     }
-    
+    return (map.size === 0)
+
 };
