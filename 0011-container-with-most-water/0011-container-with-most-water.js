8b7780372a70b4825625f22910bbc3d5e3274a4d
Sayantani Manna <sayantanimanna139@gmail.com>
2026-08-24 22:10:55 +0530

    Time: 7 ms (5.52%), Space: 62.6 MB (71.03%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let left = 0
+    let right = height.length - 1
+    let maxArea = 0
+    while (left < right) {
+        let area = (right - left) * (Math.min(height[left], height[right]))
+        if (area > maxArea) {
+            maxArea = area
+        }
+        if (height[left] < height[right]) {
+            left++ //move the shorter pointer
+        } else {
+            right--
+        }
+    }
+    return maxArea
+
+};
