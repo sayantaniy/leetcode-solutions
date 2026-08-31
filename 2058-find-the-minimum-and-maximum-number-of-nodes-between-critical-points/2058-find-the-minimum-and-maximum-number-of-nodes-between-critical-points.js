58894a1033a7195015032ffdf866ae7e52a47687
Sayantani Manna <sayantanimanna139@gmail.com>
2026-08-31 20:55:50 +0530

    Time: 20 ms (50%), Space: 86.2 MB (71.15%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
@@ -11,22 +11,22 @@
  */
 var nodesBetweenCriticalPoints = function (head) {
     let critArr = [] //array saving the node number of the CP
-    let curr = head
-    let dummy = new ListNode(0, head)
-    let prev = dummy
-    let position = 1
+    let prev = head
+    let curr = head.next //current starts from position 2, can never be head
+
+    let position = 2
     while (curr && curr.next) {
-        if (prev !== dummy && curr.val > curr.next.val && curr.val > prev.val) {
+        if (curr.val > curr.next.val && curr.val > prev.val) {
             critArr.push(position)
         }
-        if (prev !== dummy && curr.val < curr.next.val && curr.val < prev.val) {
+        if (curr.val < curr.next.val && curr.val < prev.val) {
             critArr.push(position)
         }
         prev = curr
         curr = curr.next
         position += 1
     }
-    critArr.sort((a, b) => a - b)
+    // critArr.sort((a, b) => a - b) [from left to right] position is already sorted, so no need of them 
     if (critArr.length < 2) return [-1, -1]
     let maxD = critArr[critArr.length - 1] - critArr[0]
     let minD = Infinity
