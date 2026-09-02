3b765e42e0615f2cfeb54d3057e2de3b969770d8
Sayantani Manna <sayantanimanna139@gmail.com>
2026-09-02 22:50:15 +0530

    Time: 0 ms (100%), Space: 55.4 MB (78.13%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function(nums1) {
+    //3 situations
+    //all odd
+    //all even
+    //mix of odd and even, even-odd=odd, so an odd array can always be contructed
+    //always true
+    return true
+};
