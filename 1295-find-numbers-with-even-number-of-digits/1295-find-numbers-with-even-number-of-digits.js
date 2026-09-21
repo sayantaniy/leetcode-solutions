4b2974f677d12404ef42e35020b0cb441a897c3e
Sayantani Manna <sayantanimanna139@gmail.com>
2026-09-22 00:32:08 +0530

    Time: 3 ms (25.41%), Space: 55.4 MB (39.55%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -2,21 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findNumbers = function (nums) {
-    let map = new Map()
-    let count = 0
-    for (let num of nums) {
-        if (!map.get(num))
-        map.set(num, String(num).length)
-        else{
-            if ((String(num).length)%2===0)
-            count+=1
-        }
+var findNumbers = function(nums) {
+    let c = 0
+    for(let n of nums){
+        if(String(n).length%2===0)
+        c+=1
     }
-    for (let [key, val] of map) {
-        if (val % 2 === 0)
-            count+=1
-    }
-
-    return count
+    return c
 };
